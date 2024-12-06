7be948c70814e33df10cd4856719ec9c213a215d
Beytullah <beytul.1907_fb@icloud.com>
2024-12-06 22:17:22 +0300

    Veri_Girişi_Alma

--- ogrenci-bilgi-sistemi.py
+++ ogrenci-bilgi-sistemi.py
@@ -21,10 +21,61 @@ def ogrBilgi(ad):
     veri = safe.execute("Select * from ogrenci WHERE ogrenci_adi=?",(ad,));
     print(veri.fetchall())    
 
-print("İsim giriniz:")
-isim = input()
+def yeniOgrenci(no,ad,soyad):
+    ekle = safe.execute("INSERT INTO ogrenci(ogrenci_no,ogrenci_adi,ogrenci_soyadi) VALUES(?,?,?)",(no,ad,soyad));
+    if ekle:
+        print("Kayıt Başarılı");
+    else:
+        print("Kayıt Başarısız ");
 
-ogrBilgi(isim)
+def dersNotEkle(no,mat,fiz,biy):
+    sonuc = safe.execute("INSERT INTO notlar(ogrenci_no,matematik,fizik,biyoloji) VALUES(?,?,?,?)",(no,mat,fiz,biy));
+    if sonuc:
+        print("Ders Notu Ekleme Başarılı..")
+    else :
+        print("İşlem Başarısız")
+
+def devamsizlikEkle(no,devamsizlik):
+    cikti = safe.execute("INSERT INTO devamsizlik(ogrenci_no,devamsizlik) VALUES (?,?)",(no,devamsizlik));
+    if cikti:
+        print("Veri Ekleme Başarılı")
+    else:
+        print("İşlem Başarısız")
+print("Yapmak istediğiniz işlemi giriniz:");
+islem = int(input())
+
+if (islem>0 and islem<5):
+    if(islem ==1):
+        print("Bigilerini öğrenmek istediğiniz öğrencinin adını girin:");
+        isim = input();
+        ogrBilgi(isim);   
+    if(islem==2):
+        print("Eklemek istediğiniz öğrencinin numarasını girin:")
+        no= input()
+        print("Eklemek istediğiniz öğrencinin adını girin:")     
+        ad = input()
+        print("Eklemek istediğiniz öğrencinin soyadını girin:")
+        soyad = input()
+        yeniOgrenci(no,ad,soyad)
+    if(islem ==3):
+        print("Eklemek istediğiniz notdaki öğrencinin numarasını girin:")
+        no= input()
+        print("Eklemek istediğiniz öğrencinin matematik notunu girin:")     
+        mat = input()
+        print("Eklemek istediğiniz öğrencinin fizik notunu girin:")
+        fiz = input()             
+        print("Eklemek istediğiniz öğrencinin biyoloji notunu girin:")
+        biy = input()
+        dersNotEkle(no,mat,fiz,biy);
+    if(islem == 4):
+        print("Devamsızlığı olan öğrencinin numarasını girin:")
+        no = input()
+        print("Öğrencinin devamsızlık sayısını girin:")     
+        dev = input()
+        devamsizlikEkle(no,dev)
+else:
+    print("Hatalı bir sayı girdiniz.");
+
 
 
 connect.commit()
